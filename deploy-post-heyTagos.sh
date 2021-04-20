eabfad4f07aca4bbc4005265fee7c7e8af17ff4c
yuya-okabe <36470040+okabe-yuya@users.noreply.github.com>
2021-04-20 10:29:34 +0900

    Delete deploy-post-heyTagos.sh

--- deploy-post-heyTagos.sh
+++
@@ -1,8 +0,0 @@
-gcloud functions deploy postHeyTagos \
-  --entry-point PostTagosHttpServer \
-  --runtime go113 \
-  --trigger-http \
-  --allow-unauthenticated \
-  --memory 128MB \
-  --region asia-northeast2 \
-  --set-env-vars ENV_NAME=production

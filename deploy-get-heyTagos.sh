2d34bdfcbd10921ef50997431e9673c2866dffc9
yuya-okabe <36470040+okabe-yuya@users.noreply.github.com>
2021-04-20 10:29:42 +0900

    Delete deploy-get-heyTagos.sh

--- deploy-get-heyTagos.sh
+++
@@ -1,8 +0,0 @@
-gcloud functions deploy getHeyTagos \
-  --entry-point GetTagosHttpServer \
-  --runtime go113 \
-  --trigger-http \
-  --allow-unauthenticated \
-  --memory 128MB \
-  --region asia-northeast2 \
-  --set-env-vars ENV_NAME=production
